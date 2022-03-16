cccde4021fda6fb0edfe7c38017bd78c2352fcc6
Rekha <erina.rekha@icloud.com>
2022-03-16 15:17:28 -0700

    Add view Will Appear

--- Udder/ViewController.swift
+++ Udder/ViewController.swift
@@ -14,6 +14,8 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
-
+    override func viewWillAppear(_ _animated: Bool) { 
+        // Do any additional setup after loading the view.
+    }
 }
 
